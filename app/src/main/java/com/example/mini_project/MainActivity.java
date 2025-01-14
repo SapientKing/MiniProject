d5133e080184b110ff347bfc52f27ae6acd88a2a
Ugahnesh Moorthy <Ugah0603@gmail.com>
2025-01-15 02:45:59 +0800

    done by ugah

---
+++ app/src/main/java/com/example/mini_project/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.mini_project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.txtpassword);
+
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+
+        //admin and admin
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    Intent intent = new Intent(getApplicationContext(), ViewAction.class);
+                    startActivity(intent);
+                    Toast.makeText(MainActivity.this, "LOGIN SUCCESSFULL", Toast.LENGTH_SHORT).show();
+
+                } else
+                    Toast.makeText(MainActivity.this, "LOGIN FAILED PLEASE TRY AGAIN !!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/mini_project/ViewAction.java
@@ -0,0 +1,45 @@
+package com.example.mini_project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ViewAction extends AppCompatActivity {
+    private CardView addItems, deleteItems, scanItems, viewInventory;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_action);
+
+
+        addItems = (CardView) findViewById(R.id.addItems);
+        deleteItems = (CardView) findViewById(R.id.deleteItems);
+        //scanItems = (CardView) findViewById(R.id.scanItems);
+        viewInventory = (CardView) findViewById(R.id.viewInventory);
+
+        //addItems.setOnClickListener(this);
+        //deleteItems.setOnClickListener(this);
+        //scanItems.setOnClickListener(this);
+        //viewInventory.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        Intent i;
+
+        switch (view.getId()) {
+            //case R.id.addItems : i = new Intent(this,AddProduct.class); startActivity(i); break;
+            //case R.id.deleteItems : i = new Intent(this,DeleteProduct.class);startActivity(i); break;
+            //case R.id.scanItems : i = new Intent(this,ViewProduct.class);startActivity(i); break;
+            //case R.id.viewInventory : i = new Intent(this,ViewInventory.class);startActivity(i); break;
+            default: break;
+        }
+    }
+}
